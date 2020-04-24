e9a74e213646fcb69c07acc8e78572f9e1946a9d
Rudresh Pratap Singh <rudreshsingh999@gmail.com>
2020-04-24 17:57:07 +0530

    Few Changes

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.app2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setTitle("BiteSize");
         setContentView(R.layout.activity_main);
-
+        
 
         signIn = (Button) findViewById(R.id.btnSignIn);
         signUp = (Button) findViewById(R.id.btnSignUp);
